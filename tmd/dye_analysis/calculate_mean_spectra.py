38caab50900ead73adc32a43c5e074625b1045e8
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2024-03-04 10:10:25 +0100

    Changed mean spectrum calculations to include multiple measurements

--- tmd/dye_analysis/calculate_mean_spectra.py
+++ tmd/dye_analysis/calculate_mean_spectra.py
@@ -3,21 +3,25 @@ import glob
 import numpy as np
 from tmd.utils.io_iad_results import load_iad_results
 
-dye_base_dir = "/home/kris/Work/Data/TMD/DyeSpectra"
+dye_base_dir = "/home/kris/Data/Dye_project/Measured_Spectra/"
 excluded_phantoms = ["B01A", "B01B", "B02A", "B02B", "B03A", "B03B", "B13A", "B13B", "B08A", "B08B", "B24A"]
 
 spectrum_files = glob.glob((os.path.join(dye_base_dir, "*", "B*", "*.npz")))
-spectrum_files = [file for file in spectrum_files if os.path.basename(file).split(".")[0] not in excluded_phantoms]
+spectrum_files = [file for file in spectrum_files if "BF" in os.path.basename(file).split(".")[0]]
 
-unique_phantom_names = sorted(list(set([os.path.basename(file).split(".")[0][:3] for file in spectrum_files])))
+unique_phantom_names = sorted(list(set([os.path.basename(file).split(".")[0][:-1] for file in spectrum_files])))
 
 for phantom_name in unique_phantom_names:
     files = [file for file in spectrum_files if phantom_name in file]
-
+    if phantom_name == "BF10":
+        for f_idx, f in enumerate(sorted(files)):
+            save_dict = load_iad_results(f)
+            np.savez(os.path.join(dye_base_dir, f"{phantom_name}{['A', 'B', 'C'][f_idx]}.npz"), **save_dict)
+        continue
     if len(files) == 1:
         save_dict = load_iad_results(files[0])
 
-    else:
+    elif len(files) == 2:
         data_dict_A = load_iad_results(files[0])
         data_dict_B = load_iad_results(files[1])
 
@@ -28,6 +32,18 @@ for phantom_name in unique_phantom_names:
             else:
                 save_dict[key_A] = (value_A + value_B)/2
 
-    np.savez(os.path.join(dye_base_dir, "Measured_Spectra", f"{phantom_name}.npz"), **save_dict)
+    elif len(files) == 3:
+        data_dict_A = load_iad_results(files[0])
+        data_dict_B = load_iad_results(files[1])
+        data_dict_C = load_iad_results(files[2])
+
+        save_dict = dict()
+        for (key_A, value_A), (key_B, value_B), (key_C, value_C) in zip(data_dict_A.items(), data_dict_B.items(), data_dict_C.items()):
+            if "std" in key_A:
+                save_dict[key_A] = 1/3 * np.sqrt(value_A**2 + value_B**2 + value_C**2)
+            else:
+                save_dict[key_A] = (value_A + value_B + value_C)/3
+
+    np.savez(os.path.join(dye_base_dir, f"{phantom_name}.npz"), **save_dict)
 
 
