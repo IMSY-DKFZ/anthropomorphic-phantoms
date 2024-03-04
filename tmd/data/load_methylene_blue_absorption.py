63d7ebdc873c13cdad624531fe7582c00b22c2d3
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2024-03-04 10:06:51 +0100

    Added methylene blue as absorber

---
+++ tmd/data/load_methylene_blue_absorption.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+
+
+def load_mb():
+    file_path = "/home/kris/Work/Repositories/tissue-mimicking-dyes/tmd/data/methylene_blue.csv"
+    wavelengths = np.loadtxt(file_path, skiprows=2, usecols=0)
+    mua = 2.303 * np.loadtxt(file_path, skiprows=2, usecols=1)
+    return wavelengths, gaussian_filter1d(mua, 2)
