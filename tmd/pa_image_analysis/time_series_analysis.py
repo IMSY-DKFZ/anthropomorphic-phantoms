5c83a4713e89d8fbd0eb130330d2f4d025c7cb4f
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2024-09-03 11:46:40 +0200

    Added time series analysis

---
+++ tmd/pa_image_analysis/time_series_analysis.py
@@ -0,0 +1,10 @@
+import simpa as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+base_path = "/home/kris/Data/Dye_project/PAT_Data/Example_reconstructions_das"
+
+dirs = os.listdir(base_path)
+print(dirs)
