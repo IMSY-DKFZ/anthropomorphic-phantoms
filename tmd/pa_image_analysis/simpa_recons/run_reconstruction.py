c4dc38830ca45126e219afb751a4488eba9ae1ee
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2025-01-22 13:55:38 +0100

    Run reconstruction with custom device

--- tmd/pa_image_analysis/simpa_recons/run_reconstruction.py
+++ tmd/pa_image_analysis/simpa_recons/run_reconstruction.py
@@ -11,9 +11,9 @@ import pandas as pd
 import json
 
 
-from tmd.utils.default_settings import (get_default_das_reconstruction_settings,
-                                        get_default_tr_reconstruction_settings)
+from tmd.utils.default_settings import (get_default_das_reconstruction_settings)
 from tmd.utils.recon_utils import correct_er_sensors
+from tmd.simulations.pat.custom_msot_acuity import MSOTAcuityEcho
 
 from simpa.core.simulation_modules.reconstruction_module.reconstruction_utils import \
     tukey_bandpass_filtering_with_settings
@@ -25,7 +25,7 @@ from simpa.core.simulation_modules.reconstruction_module.reconstruction_utils im
 # base_path = "/home/kris/Data/Dye_project/PAT_Data/iThera_data/Processed_Data/"
 # base_path = "/home/kris/Data/Dye_project/PAT_Data/Depth_test_data/Processed_data"
 base_path = "/home/kris/Data/Dye_project/PAT_Data/iThera_2_data/Processed_Data"
-frame_averaging = (9, 15)
+frame_averaging = (9, 13)
 studies = sorted(glob.glob(os.path.join(base_path, "Study_*")))
 recon_settings = get_default_das_reconstruction_settings()
 wavelengths = np.arange(700, 851, 10)
@@ -47,8 +47,12 @@ general_settings = {
 #                    ("Study_9", "Scan_4"), ("Study_10", "Scan_8"), ("Study_11", "Scan_19")]
 
 # examples_images = [("Study_17", "Scan_1"), ("Study_18", "Scan_1"), ("Study_19", "Scan_1"), ("Study_20", "Scan_1")]
-# examples_images = [("Study_28", "Scan_1"), ("Study_28", "Scan_"), ("Study_19", "Scan_1"), ("Study_20", "Scan_1")]
-examples_images = [("Study_27", "Scan_17"), ("Study_27", "Scan_18"), ("Study_27", "Scan_19")]
+examples_images = [("Study_25", "Scan_25"),
+                   ("Study_26", "Scan_12"),
+                   ("Study_27", "Scan_18"),
+                   ("Study_28", "Scan_6"),
+                   ("Study_31", "Scan_9")]
+# examples_images = [("Study_27", "Scan_19")]
 
 # study_nrs = np.arange(61, 67, 1)
 # vessel_scan_nrs = pd.read_csv("/home/kris/Downloads/Forearm Motion Annotation - Tabellenblatt1.csv", header=[0, 1])
@@ -72,11 +76,18 @@ for study in studies:
         if not any([(st in scan) and (sc in scan) for st, sc in examples_images]):
             continue
         scan_nr = scan.split("_")[-1][:-5]
+        if scan_nr == "1" or int(scan_nr) == len(scans):
+            continue
         print(scan)
         # frame_averaging = tuple(vessel_frames[f"Study_{study_nr}"][f"Scan_{scan_nr}"])
         pa_data = pat.PAData.from_hdf5(scan)
 
         time_series = pa_data.get_time_series().raw_data
+        # us_image = pa_data.get_ultrasound().raw_data
+        # plt.imshow(np.flipud(np.squeeze(us_image[16, 10, ...])))
+        # plt.show()
+        # print(us_image)
+        # exit()
         nr_frames = time_series.shape[0]
         if not frame_averaging:
             frame_ranges = range(0, nr_frames)
@@ -85,11 +96,14 @@ for study in studies:
                 middle_frame = int(nr_frames / 2)
                 frame_ranges = range(middle_frame - frame_averaging // 2, middle_frame + frame_averaging // 2 + 1)
             else:
-                frame_ranges = range(frame_averaging[0], frame_averaging[1] + 1)
+                end_frame = frame_averaging[1] if frame_averaging[1] < nr_frames else nr_frames - 1
+                frame_ranges = range(frame_averaging[0], end_frame + 1)
 
         frames = list()
         corr_fac = pa_data.get_overall_correction_factor()
-        for frame in frame_ranges:
+        for fr_idx, frame in enumerate(frame_ranges):
+            if frame == 0 or frame == nr_frames - 1:
+                continue
 
             correction_factors = corr_fac[frame, :, None, None]
             time_series_corr = time_series[frame, ...] / correction_factors
@@ -101,7 +115,7 @@ for study in studies:
                                                               filter_func=bandpass_filter), 2, 0)
 
             save_path = scan.replace("Processed_Data", "Reconstructions_das")
-            save_path = save_path.replace(".hdf5", "_recon.hdf5")
+            save_path = save_path.replace(".hdf5", "_recon1.hdf5")
             dirname = os.path.dirname(save_path)
             settings[Tags.SIMULATION_PATH] = dirname
             settings[Tags.VOLUME_NAME] = os.path.basename(save_path).split(".")[0]
@@ -114,7 +128,7 @@ for study in studies:
                                file_path=save_path,
                                data_field=Tags.DATA_FIELD_TIME_SERIES_DATA)
 
-            device = sp.MSOTAcuityEcho(field_of_view_extent_mm=np.array([-20, 20, 0, 0, -4, 16]))
+            device = MSOTAcuityEcho(field_of_view_extent_mm=np.array([-20, 20, 0, 0, 0, 20]))
             recon = sp.DelayAndSumAdapter(settings)
 
             sp.simulate(simulation_pipeline=[recon], digital_device_twin=device, settings=settings)
