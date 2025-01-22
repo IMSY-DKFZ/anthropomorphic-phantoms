bc3927d5414e5ed37aecee1cf826d0d8fc254598
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2025-01-22 13:54:57 +0100

    Modified PAT examples

--- tmd/pa_image_analysis/example_pat_images.py
+++ tmd/pa_image_analysis/example_pat_images.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
 import os
 import simpa as sp
-from tmd.utils.io_iad_results import load_iad_results
+from tmd.utils.correlate_spectrum_to_oxies import correlate_spectrum
+from tmd.utils.maximum_x_percent_values import top_x_percent_indices
+from tmd.utils.generate_phantom_file_paths import generate_file_paths
 from tmd.linear_unimxing import unmix_so2_proxy
 import nrrd
 from matplotlib_scalebar.scalebar import ScaleBar
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.lines import Line2D
+from typing import Union
+import json
 
 recon_method = "das"
 # base_path = f"/home/kris/Data/Dye_project/PAT_Data/Example_reconstructions_{recon_method}/"
@@ -16,13 +19,9 @@ base_path = f"/home/kris/Data/Dye_project/PAT_Data/iThera_2_data/Reconstructions
 
 measurements_path = "/home/kris/Data/Dye_project/Measured_Spectra"
 
-abs_dict = {
-    0: "B90",
-    0.3: "B93",
-    0.5: "B95",
-    0.7: "B97",
-    1: "BIR",
-}
+roi: int = 5
+normalize: int = 1
+average_phantom: int = 0
 
 # examples_images = {
 #     # 1: {"oxy": 0.5, "path": os.path.join("Study_6", "Scan_22.hdf5")},
@@ -57,147 +56,126 @@ abs_dict = {
 
 # ithera2 images
 examples_images = {
-    1: {"oxy": 0.5, "path": os.path.join("Study_25", "Scan_25_recon.hdf5")},
-    2: {"oxy": 0.3, "path": os.path.join("Study_26", "Scan_12_recon.hdf5")},
-    3: {"oxy": 0, "path": os.path.join("Study_27", "Scan_18_recon.hdf5")},
-    4: {"oxy": 0.7, "path": os.path.join("Study_28", "Scan_6_recon.hdf5")},
-    5: {"oxy": 1, "path": os.path.join("Study_31", "Scan_9_recon.hdf5")},
-    # 6: {"oxy": 1, "path": os.path.join("Study_32", "Scan_21_recon.hdf5")},
+    1: {"oxy": 0.5, "path": os.path.join("Study_25", "Scan_25_recon1.hdf5")},
+    2: {"oxy": 0.3, "path": os.path.join("Study_26", "Scan_12_recon1.hdf5")},
+    3: {"oxy": 0, "path": os.path.join("Study_27", "Scan_19_recon1.hdf5")},
+    4: {"oxy": 0.7, "path": os.path.join("Study_28", "Scan_5_recon1.hdf5")},
+    5: {"oxy": 1, "path": os.path.join("Study_31", "Scan_9_recon1.hdf5")},
+    # 6: {"oxy": 1, "path": os.path.join("Study_32", "Scan_21_recon1.hdf5")},
+}
+
+oxy_dict = {
+    0: 4,
+    0.3: 5,
+    0.5: 6,
+    0.7: 7,
+    1: 8
 }
 
 for forearm_nr, forearm_specs in examples_images.items():
-    path = os.path.join(base_path, forearm_specs["path"])
-    wavelengths = sp.load_data_field(path, sp.Tags.SETTINGS)[sp.Tags.WAVELENGTHS][:]
-    # print(wavelengths)
-    training_labels = np.rot90(sp.load_data_field(path, sp.Tags.DATA_FIELD_SEGMENTATION), 3)
-    reconstruction = sp.load_data_field(path, sp.Tags.DATA_FIELD_RECONSTRUCTED_DATA)
-    reconstruction_array = np.stack([np.rot90(reconstruction[str(wl)][:, :, ...], 3) for wl in wavelengths])
-    # print(reconstruction_array.shape)
+    print(forearm_nr)
+    main_phantom_path = os.path.join(base_path, forearm_specs["path"])
+    if average_phantom:
+        path_list = generate_file_paths(main_phantom_path)
+    else:
+        path_list = [main_phantom_path]
+    spectra_to_unmix = list()
+    for p_idx, path in enumerate(path_list):
+        wavelengths = sp.load_data_field(path, sp.Tags.SETTINGS)[sp.Tags.WAVELENGTHS][1:]
+        # print(wavelengths)
+        # training_labels = np.rot90(sp.load_data_field(path, sp.Tags.DATA_FIELD_SEGMENTATION), 3)
+        z_det_pos = 152
+        training_labels = np.rot90(nrrd.read(path.replace("Reconstructions_das", "US_analysis").replace("recon1.hdf5", "pa-labels.nrrd"))[0], 3)
+        training_labels = np.squeeze(np.fliplr(training_labels)[z_det_pos:z_det_pos + 200, :])
+        label = oxy_dict[forearm_specs["oxy"]]
+        training_labels[training_labels != label] = 0
+        training_labels[training_labels == label] = 1
 
-    # absorption = sp.load_data_field(path, sp.Tags.DATA_FIELD_ABSORPTION_PER_CM)
-    # absorption_array = np.stack([np.rot90(absorption[str(wl)], 3) for wl in wavelengths])
+        reconstruction = sp.load_data_field(path, sp.Tags.DATA_FIELD_RECONSTRUCTED_DATA)
+        reconstruction_array = np.stack([np.rot90(reconstruction[str(wl)][:, :, ...], 3) for wl in wavelengths])
 
-    # plt.subplot(3, 1, 1)
-    # plt.imshow(reconstruction_array[4])
-    # plt.subplot(3, 1, 2)
-    # plt.imshow(reconstruction_array[5])
-    # plt.subplot(3, 1, 3)
-    # plt.imshow(reconstruction_array[5] - reconstruction_array[4])
-    # plt.show()
-    # training_labels, _ = nrrd.read(path.replace(".hdf5", "_image-labels.nrrd"))
-    # training_labels = training_labels[0]
+        fig = plt.figure(figsize=(8, 7))
+        plt.subplot(2, 1, 1)
+        im = plt.imshow(reconstruction_array[0], vmin=0, vmax=43.3)
+        ax = plt.gca()
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        scalebar = ScaleBar(0.1, "mm")
+        ax.add_artist(scalebar)
+        plt.contour(training_labels)
+        plt.title('Image and ROI boundaries')
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="2%", pad=0.05)
+        plt.colorbar(im, cax=cax, orientation="vertical")
 
-    # training_labels = np.zeros(reconstruction_array.shape[1:], dtype=np.uint8)
-    # print(training_labels.shape)
-    # training_labels[42:45, 195:198] = 3     # for example image 5
-    # training_labels[61:63, 255:257] = 3     # for example image 5
-    # training_labels[53:55, 204:206] = 3     # for example image 3
-    # training_labels[41:45, 194:197] = 3     # for sim image 1
-    # training_labels[39:41, 201:203] = 3     # for sim image 3
-    # training_labels[42:45, 244:247] = 3     # for sim image 5
+        if roi in [100, 0]:
+            vessel = reconstruction_array[:, training_labels == 1]
+        else:
+            indices = top_x_percent_indices(reconstruction_array[10], training_labels, roi)
+            # print("Indices of the top 20% maximum values:", indices)
 
-    # training_labels[65:68, 190:193] = 3  # for ithera image 3: 17
-    # training_labels[65:69, 145:149] = 3  # for ithera image 3: 16
-    # training_labels[65:75, 183:207] = 3  # for ithera image 3: 18   sos 1500
-    # training_labels[62:65, 189:200] = 3  # for ithera image 3: 18   sos 1500
-    # training_labels[63:77, 139:161] = 3  # for ithera image 1: 24   sos 1500
-    # training_labels[59:63, 144:156] = 3  # for ithera image 1: 24   sos 1500
-    # training_labels[63:69, 266:271] = 3  # for ithera image 4
-    # nrrd.write(os.path.join(path, f"{forearm_specs['path']}-roi.nrrd"), training_labels)
-    # # training_labels, _ = nrrd.read(os.path.join(path, f"{forearm_specs['path']}-roi.nrrd"))
+            seg_array = np.zeros_like(reconstruction_array[10])
+            maximum_value_pixels = list()
+            for idx in indices:
+                maximum_value_pixels.append(reconstruction_array[:, idx[0], idx[1]])
+                seg_array[idx[0], idx[1]] = 1
 
-    # nrrd.write(path.split('.')[0] + "_image.nrrd", reconstruction_array)
-    #
-    fig = plt.figure(figsize=(8, 7))
-    plt.subplot(2, 1, 1)
-    im = plt.imshow(reconstruction_array[0], vmin=0, vmax=43.3)
-    ax = plt.gca()
-    ax.axes.xaxis.set_visible(False)
-    ax.axes.yaxis.set_visible(False)
-    scalebar = ScaleBar(0.1, "mm")
-    ax.add_artist(scalebar)
-    plt.contour(training_labels)
-    plt.title('Image, mask and ROI boundaries')
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="2%", pad=0.05)
-    plt.colorbar(im, cax=cax, orientation="vertical")
-    # plt.tight_layout()
-    # plt.show()
-    # exit()
+            vessel = np.array(maximum_value_pixels)
+            vessel = np.moveaxis(vessel, 0, 1)
+        print(np.mean(vessel, axis=0).shape)
+        print(vessel.shape)
+        if normalize:
+            # vessel = vessel - np.mean(vessel, axis=0)
+            vessel_norm = np.linalg.norm(vessel, axis=0, ord=1)
+            vessel_spectrum = vessel / vessel_norm[np.newaxis, :]
+        else:
+            vessel_spectrum = np.squeeze(vessel)
+        if len(vessel_spectrum.shape) > 1:
+            vessel_std = np.std(vessel_spectrum, axis=1)
+            vessel_spectrum = np.mean(vessel_spectrum, axis=1)
 
+        slope, intercept, r_value, p_value, std_err, target_spectrum, target_std = correlate_spectrum(
+            vessel_spectrum,
+            wavelengths=wavelengths,
+            oxy=forearm_specs["oxy"])
 
-    vessel = reconstruction_array[:, training_labels == 1]
-    # abs_vessel = absorption_array[:, training_labels == 3]
-    # abs_vessel = np.squeeze(abs_vessel)
-    # abs_vessel = np.mean(abs_vessel, axis=1)
-    print(np.mean(vessel, axis=0).shape)
-    print(vessel.shape)
-    vessel = vessel - np.mean(vessel, axis=0)
-    vessel_norm = np.linalg.norm(vessel, axis=0, ord=1)
-    vessel_spectrum = vessel / vessel_norm[np.newaxis, :]
-    # vessel_spectrum = np.squeeze(vessel)
-    if len(vessel_spectrum.shape) > 1:
-        vessel_std = np.std(vessel_spectrum, axis=1)
-        vessel_spectrum = np.mean(vessel_spectrum, axis=1)
-    hb_abs_spectrum = load_iad_results(os.path.join(measurements_path, "B90.npz"))["mua"]
-    hb_abs_spectrum = np.interp(wavelengths, np.arange(650, 950), hb_abs_spectrum)
+        json.dump({"slope": slope, "intercept": intercept, "r_value": r_value, "p_value": p_value, "std_err": std_err},
+                  open(path.replace("hdf5", "json"), "w"))
 
-    hbo2_abs_spectrum = load_iad_results(os.path.join(measurements_path, "BIR.npz"))["mua"]
-    hbo2_abs_spectrum = np.interp(wavelengths, np.arange(650, 950), hbo2_abs_spectrum)
+        plt.subplot(2, 1, 2)
 
-    target_spectrum = load_iad_results(os.path.join(measurements_path, abs_dict[forearm_specs["oxy"]] + ".npz"))["mua"]
-    target_spectrum = np.interp(wavelengths, np.arange(650, 950), target_spectrum)
+        plt.title(
+            f"Target spectrum (oxy={int(100 * forearm_specs['oxy']):d}%) with unmixed oxy: "
+            f"{unmix_so2_proxy(vessel_spectrum * slope + intercept, wavelengths=wavelengths):.2f} % and "
+            f"{unmix_so2_proxy(vessel_spectrum, wavelengths=wavelengths):.2f} % (uncorrelated)")
+        plt.ylabel("Absorption coefficient [$cm^{-1}$]")
+        plt.xlabel("Photoacoustic signal [a.u.]")
+        plt.scatter(vessel_spectrum, target_spectrum, color="green", label=f"Measured PA signal")
+        plt.plot(vessel_spectrum, vessel_spectrum * slope + intercept, color="black",
+                 label=f"Correlation (R={r_value:.2f}, p-value={p_value:.2f})")
 
-    target_std = load_iad_results(os.path.join(measurements_path, abs_dict[forearm_specs["oxy"]] + ".npz"))["mua_std"]
-    target_std = np.interp(wavelengths, np.arange(650, 950), target_std)
+        ax = plt.gca()
+        ins = ax.inset_axes((0.7, 0.2, 0.2, 0.2))
+        ins.plot(wavelengths, vessel_spectrum * slope + intercept, color="green")
+        ins.plot(wavelengths, target_spectrum, label="Measured material absorption", color="blue")
+        ins.fill_between(wavelengths, target_spectrum - target_std,
+                         target_spectrum + target_std,
+                         alpha=0.2, color="blue")
+        ins.set_title("PAT approx of $\mu_a$")
+        ins.set_ylabel("$\mu_a$ [$cm^{-1}$]")
+        ins.set_xlabel("Wavelength [nm]")
 
-    slope, intercept, r_value, p_value, std_err = linregress(target_spectrum, vessel_spectrum)
-    print(slope, intercept, r_value, p_value, std_err)
+        lgd = plt.legend(loc="upper left")
+        ax = lgd.axes
+        handles, labels = ax.get_legend_handles_labels()
+        handles.append(Line2D(xdata=[0], ydata=[0], color="blue"))
+        labels.append("Measured material absorption")
+        lgd._legend_box = None
+        lgd._init_legend_box(handles, labels)
+        lgd._set_loc(lgd._loc)
+        lgd.set_title(lgd.get_title().get_text())
+        fig.tight_layout()
 
-    # plt.subplot(2, 2, 3)
-    # plt.title('Unmixing result')
-    # im = plt.imshow(unmix_so2_proxy(intercept + slope * reconstruction_array, wavelengths))
-    # # plt.imshow(absorption_array[10], alpha=0.2)
-    # plt.contour(training_labels)
-    # plt.colorbar(im)
-
-    # print("vessel spectrum", intercept + slope * vessel_spectrum)
-
-    plt.subplot(2, 1, 2)
-
-    plt.title(
-        f"Target Spectrum (oxy={int(100 * forearm_specs['oxy']):d}%) with unmixed oxy: "
-        f"{unmix_so2_proxy((vessel_spectrum - intercept)/slope, wavelengths=wavelengths):.2f} % and "
-        f"{unmix_so2_proxy(vessel_spectrum, wavelengths=wavelengths):.2f} % (uncorrelated)")
-    plt.xlabel(f"Absorption Coefficient [$cm^{-1}$]")
-    plt.ylabel(f"Photoacoustic Signal [a.u.]")
-    plt.errorbar(target_spectrum, (vessel_spectrum - intercept)/slope, xerr=target_std, yerr=vessel_std, color="green", barsabove=True, ls="none", fmt="o",
-                 label=f"Measured PA signal")
-    plt.plot(target_spectrum, target_spectrum, color="black",
-             label=f"Correlation (R={r_value:.2f}, p-value={p_value:.2f})")
-
-    ax = plt.gca()
-    ins = ax.inset_axes((0.7, 0.2, 0.2, 0.2))
-    ins.plot(wavelengths, (vessel_spectrum - intercept)/slope, color="green")
-    ins.plot(wavelengths, target_spectrum, label="Measured material absorption", color="blue")
-    ins.fill_between(wavelengths, target_spectrum - target_std,
-                     target_spectrum + target_std,
-                     alpha=0.2, color="blue")
-    ins.set_title("PA spectrum of ROI")
-    ins.set_ylabel(f"$\mu_a$ [$cm^{-1}$]")
-    ins.set_xlabel("Wavelength [nm]")
-
-    lgd = plt.legend(loc="upper left")
-    ax = lgd.axes
-    handles, labels = ax.get_legend_handles_labels()
-    handles.append(Line2D(xdata=[0], ydata=[0], color="blue"))
-    labels.append("Measured material absorption")
-    lgd._legend_box = None
-    lgd._init_legend_box(handles, labels)
-    lgd._set_loc(lgd._loc)
-    lgd.set_title(lgd.get_title().get_text())
-    fig.tight_layout()
-
-    # plt.show()
-    # plt.savefig(os.path.join("/home/kris/Pictures/Phantom_Paper_Figures/", f"PAT_spectrum_correlation_oxy_{int(100*forearm_specs['oxy']):0d}.png"), dpi=300)
-    plt.close()
+        # plt.show()
+        plt.savefig(os.path.join("/home/kris/Pictures/Phantom_Paper_Figures/", f"PAT_spectrum_correlation_oxy_{int(100*forearm_specs['oxy']):0d}_p{p_idx}.png"), dpi=300)
+        plt.close()
