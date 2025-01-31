58735a45960dcd2c1f9904fdba53ee8e79627c8b
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2025-01-31 13:51:41 +0100

    Fixed paths for hsi oxy estimation

--- ap/oxy_estimation/estimate_oxy_hsi.py
+++ ap/oxy_estimation/estimate_oxy_hsi.py
@@ -6,7 +6,7 @@ import os
 import json
 from htc import DataPath
 
-from ap.oxy_estimation.oxy_estimator_base import LinearUnmixingOxyEstimator
+from ap.oxy_estimation.linear_unmixing import LinearUnmixingOxyEstimator
 
 if __name__ == "__main__":
     base_path = "/home/kris/Data/Dye_project/HSI_Data/"
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     examples_images = {
         1: {"oxy": 0.5, "path": "2024_02_20_15_28_13"},
-        2: {"oxy": 0.3, "path": f"2024_02_20_15_44_02"},
+        2: {"oxy": 0.3, "path": "2024_02_20_15_44_02"},
         3: {"oxy": 0, "path": "2024_02_20_16_12_35"},
         4: {"oxy": 0.7, "path": "2024_02_20_16_24_05"},
         5: {"oxy": 1, "path": "2024_02_20_16_50_30"},
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     for forearm_nr, forearm_specs in examples_images.items():
         print(f"Processing Forearm {forearm_nr}")
         results[forearm_nr] = dict()
-        path = os.path.join(base_path, forearm_specs["path"])
+        path = os.path.join(base_path, "HSI_Data", forearm_specs["path"])
         htc_data = DataPath(path)
         rgb = htc_data.read_rgb_reconstructed()
         hsi = htc_data.read_cube()
@@ -49,9 +49,12 @@ if __name__ == "__main__":
         labels, _ = nrrd.read(os.path.join(path, f"{forearm_specs['path']}-labels.nrrd"))
         data_shape = labels.shape
 
-        oxy_estimates = estimator.estimate(hsi)
-        with open(path + ".json", "r") as json_file:
+        json_path = os.path.join(base_path, "Paper_Results", "HSI_Measurement_Correlation",
+                                 f"HSI_spectrum_correlation_oxy_{int(100 * forearm_specs['oxy']):0d}.json")
+        with open(json_path, "r") as json_file:
             regression_data = json.load(json_file)
+
+        oxy_estimates = estimator.estimate(hsi)
         cal_oxy_estimates = estimator.estimate(
             (hsi - regression_data["intercept"])/regression_data["slope"])
 
@@ -68,4 +71,9 @@ if __name__ == "__main__":
             "vessels": vessels,
             "tissue": tissue
         }
-        np.savez(f"/home/kris/Data/Dye_project/HSI_Data/Oxy_Results/forearm_{forearm_nr}.npz", **res)
+
+        save_dir = os.path.join(base_path, "Paper_Results", "Oxy_Results")
+        os.makedirs(save_dir, exist_ok=True)
+        save_path = os.path.join(save_dir, f"hsi_forearm_{forearm_nr}.npz")
+
+        np.savez(save_path, **res)
