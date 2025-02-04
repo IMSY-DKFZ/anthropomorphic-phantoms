15759ecb23272ca4502708c19b9609d79d1e6df6
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2025-02-04 14:39:33 +0100

    Fixed paths

--- ap/data/load_icg_absorption.py
+++ ap/data/load_icg_absorption.py
@@ -1,8 +1,20 @@
 import numpy as np
+import os
 
 
 def load_icg():
-    file_path = "/home/kris/Work/Repositories/tissue-mimicking-dyes/ap/data/icg.csv"
+    try:
+        run_by_bash: bool = bool(os.environ["RUN_BY_BASH"])
+        print("This runner script is invoked in a bash script!")
+    except KeyError:
+        run_by_bash: bool = False
+
+    if run_by_bash:
+        base_path = os.environ["PYTHON_PATH"]
+    else:
+        # In case the script is run from an IDE, the base path has to be set manually
+        base_path = "/path/to/anthropomorphic-phantoms/ap"
+    file_path = os.path.join(base_path, "data/icg.csv")
     wavelengths = np.loadtxt(file_path, skiprows=2, usecols=0)
     mua = 2.303 * np.loadtxt(file_path, skiprows=2, usecols=1)
     return wavelengths, mua
--- ap/data/load_methylene_blue_absorption.py
+++ ap/data/load_methylene_blue_absorption.py
@@ -1,9 +1,21 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
+import os
 
 
 def load_mb():
-    file_path = "/home/kris/Work/Repositories/tissue-mimicking-dyes/ap/data/methylene_blue.csv"
+    try:
+        run_by_bash: bool = bool(os.environ["RUN_BY_BASH"])
+        print("This runner script is invoked in a bash script!")
+    except KeyError:
+        run_by_bash: bool = False
+
+    if run_by_bash:
+        base_path = os.environ["PYTHON_PATH"]
+    else:
+        # In case the script is run from an IDE, the base path has to be set manually
+        base_path = "/path/to/anthropomorphic-phantoms/ap"
+    file_path = os.path.join(base_path, "data/methylene_blue.csv")
     wavelengths = np.loadtxt(file_path, skiprows=2, usecols=0)
     mua = 2.303 * np.loadtxt(file_path, skiprows=2, usecols=1)
     return wavelengths, gaussian_filter1d(mua, 2)
